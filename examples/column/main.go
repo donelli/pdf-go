a48e2c80325d21a71dc9fa1b08de95e0ab1167ff
Eduardo Pellenz <epellenz@tractian.com>
2024-09-04 18:41:03 -0300

    feat: add examples

---
+++ examples/column/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"pdf_go_test/core"
+	pdf "pdf_go_test/widgets"
+	"strings"
+)
+
+func main() {
+	content := pdf.Column(
+		pdf.Text(strings.Repeat("Hello ", 20), pdf.FontSize(14)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+		pdf.Text("Hello", pdf.FontSize(20)),
+	)
+
+	writer := core.NewWriter(0, 0, 0, 0)
+
+	writer.RenderWidget(content)
+	writer.GeneratePdf("column.pdf")
+}
---
+++ examples/text_customizations/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"pdf_go_test/core"
+	pdf "pdf_go_test/widgets"
+)
+
+func main() {
+	content := pdf.Column(
+		pdf.Text("Hello", pdf.FontSize(20), pdf.FontColor("#ff0000")),
+	)
+
+	writer := core.NewWriter(8, 8, 8, 8)
+
+	writer.RenderWidget(content)
+	writer.GeneratePdf("text_customizations.pdf")
+}
