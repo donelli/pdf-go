eef640801a0db524246b98d6740a377fb5b8e25a
Eduardo Pellenz <epellenz@tractian.com>
2024-09-26 23:18:44 -0300

    refactor: update HexToRGBA to a faster algorithm

--- utils.go
+++ utils.go
@@ -2,20 +2,39 @@ package tpdf
 
 import (
 	"image/color"
-	"strconv"
 )
 
-func HexToRGBA(hex string) color.RGBA {
-	values, err := strconv.ParseUint(string(hex[1:]), 16, 32)
+func HexToRGBA(s string) (c color.RGBA) {
+	c.A = 0xff
 
-	if err != nil {
-		panic(err)
+	if s[0] != '#' {
+		panic("invalid format")
 	}
 
-	return color.RGBA{
-		R: uint8(values >> 16),
-		G: uint8((values >> 8) & 0xFF),
-		B: uint8(values & 0xFF),
-		A: 255,
+	hexToByte := func(b byte) byte {
+		switch {
+		case b >= '0' && b <= '9':
+			return b - '0'
+		case b >= 'a' && b <= 'f':
+			return b - 'a' + 10
+		case b >= 'A' && b <= 'F':
+			return b - 'A' + 10
+		}
+
+		panic("invalid format")
 	}
+
+	switch len(s) {
+	case 7:
+		c.R = hexToByte(s[1])<<4 + hexToByte(s[2])
+		c.G = hexToByte(s[3])<<4 + hexToByte(s[4])
+		c.B = hexToByte(s[5])<<4 + hexToByte(s[6])
+	case 4:
+		c.R = hexToByte(s[1]) * 17
+		c.G = hexToByte(s[2]) * 17
+		c.B = hexToByte(s[3]) * 17
+	default:
+		panic("invalid format")
+	}
+	return
 }
